7207e73f3d537af04929c4eba89970c96fd20f04
trashkev <kevasorensen@gmail.com>
2023-10-29 01:48:57 -0700

    oh he wobblin

--- levels/VerletObject.gd
+++ levels/VerletObject.gd
@@ -3,14 +3,20 @@ extends Node2D
 var pointColor :Color
 var collisionRadius :float = 30.0
 var timeAccum = 0.0
-var stepTime = 0.01
+var stepTime = 0.005
 var maxStep = 0.1
 
 var startPos :Vector2 = Vector2(550,0)
 var targetPos = startPos
-var count = 30
-var spacing = 15
-	
+var count = 50
+var spacing = 10
+
+var inflateForce :float = 200
+var inflateSpeed :float = 2
+var deflateSpeed :float = 2
+var inflatePercentage :float = 0.0
+var inflating :bool = false
+var time = 0.0
 func Distance(p0:Point,p1:Point):
 	var dx = p1.x - p0.x
 	var dy = p1.y - p0.y
@@ -24,6 +30,7 @@ class Point:
 	var old_y :float
 	var mass :float
 	var pinned :bool
+	var color :Color
 	
 	var externalForce :Vector2
 	var area2d : Area2D
@@ -101,22 +108,27 @@ var sticks :Array[Stick] = [
 func Simulate():
 	#update all points
 	var externalForce :Vector2 = Vector2(0.0,0.0)
-	externalForce.y += 300 #gravity
+	externalForce.y += 100 #gravity
 	#externalForce.x += -1
-	if Input.is_action_pressed("inflate"):
-		externalForce.y -= 1000
-		#pointColor = Color.GREEN
-		pointColor = Color.RED
-	else:
-		pointColor = Color.RED
+	
+	
+	
 	if Input.is_action_pressed("inflate_left"):
 		externalForce.x -= 200
 	if Input.is_action_pressed("inflate_right"):
 		externalForce.x += 200
 		
-	for point in points:
-		point.externalForce = externalForce
-		point.Update(stepTime)
+	for i in points.size():
+		#TODO: add gradual filling per point, then continue filling rest. (uhh use modulo? floor? idk)
+		var pointPercentage = i/float(points.size())
+		#print(pointPercentage)
+		if pointPercentage < inflatePercentage:
+			points[i].color = Color.RED
+			points[i].externalForce = externalForce - Vector2(0,inflateForce + 100* sin(time*5))
+		else:
+			points[i].color = Color.DARK_RED
+			points[i].externalForce = externalForce
+		points[i].Update(stepTime)
 	
 	#update all sticks
 	for i in 10:
@@ -136,21 +148,21 @@ func AdjustCollisions():
 			if body.is_in_group("environment"):
 				var collisionShape = body.shape_owner_get_shape(0,0)
 				if collisionShape is CircleShape2D:
-					print("sirko")
+					#print("sirko")
 					var dist = body.global_position.distance_to(pointPos)
 					
 					#early out if not colliding???
 					if dist - collisionShape.radius + collisionRadius < 0:
 						break
 					var dir = (pointPos - body.position).normalized()
-					var hitPos = body.global_position + dir * (collisionShape.radius + collisionRadius)
+					var hitPos = body.global_position + dir * (collisionShape.radius + collisionRadius + 1)
 					point.x = hitPos.x
 					point.y = hitPos.y
 					
 					#point.old_x = point.x + velocity.x
 					#point.old_y = point.y + velocity.y
 				elif collisionShape is RectangleShape2D:
-					print("sqar")
+					#print("sqar")
 					var localPoint = body.to_local(Vector2(point.x,point.y))
 					
 					var half :Vector2 = collisionShape.get_size() * 0.5
@@ -184,15 +196,15 @@ func GenerateRope():
 	for i in count:
 		var point
 		if i == 0:
-			point = Point.new(startPos.x,startPos.y + i*spacing,0.5,true)
+			point = Point.new(startPos.x,startPos.y + i*spacing,0.2,true)
 		else:
-			point = Point.new(startPos.x,startPos.y + i*spacing,0.5,false)
+			point = Point.new(startPos.x,startPos.y + i*spacing,0.2,false)
 		points.append(point)
 	for i in count-1:
 		var stick
 		stick = Stick.new(points[i],points[i+1],Distance(points[i],points[i+1]))
 		sticks.append(stick)
-		
+	
 func _ready():
 	#points.append(Point.new(startPos.x,startPos.y,0.5,false))
 	GenerateRope()
@@ -211,7 +223,17 @@ func _ready():
 		add_child(area2d)
 
 func _physics_process(delta):
+	time+=delta
 	targetPos = lerp(targetPos,get_global_mouse_position(),5*delta)
+	#inflate
+	print(inflateForce - 100* sin(time*5))
+	if inflating:
+		inflatePercentage += inflateSpeed * get_process_delta_time()
+		inflatePercentage = clamp(inflatePercentage,0.0,1.0)
+	else:
+		inflatePercentage -= deflateSpeed * get_process_delta_time()
+		inflatePercentage = clamp(inflatePercentage,0.0,1.0)
+	
 	timeAccum += delta
 	timeAccum = min(timeAccum,maxStep)
 	while(timeAccum >= stepTime):
@@ -235,7 +257,18 @@ func _draw():
 	for i in count as float:
 		var colorPercent :float
 		colorPercent = i/count
-		print(colorPercent)
-		draw_circle(Vector2(points[i].x,points[i].y),collisionRadius,lerp(colorA,colorB,1-colorPercent))
+		#print(colorPercent)
+		draw_circle(Vector2(points[i].x,points[i].y),collisionRadius,points[i].color * lerp(colorA,colorB,1-colorPercent))
+	
+func _input(event):
+	if Input.is_action_pressed("inflate"):
+		inflating = true
+		
+	elif !Input.is_action_pressed("inflate"):
+		inflating = false
+	
+	
+	
+	
 	
 	
